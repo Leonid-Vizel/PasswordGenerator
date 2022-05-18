3feb3fd58da156ae82e03b369a9972b8f2a4daa9
Vizel Leonid <spartanlaska@gmail.com>
2022-05-18 15:49:23 +0300

    [MicroFix] >Переименовал класс паролей-картинок

---
+++ PasswordGenerator/ImagePassword.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace PasswordGenerator
+{
+    internal class ImagePassword
+    {
+        public Bitmap Image { get; set; }
+        public string Password { get; set; }
+        public int Id { get; set;}
+    }
+}
