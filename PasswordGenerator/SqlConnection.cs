c0fd7d424f37ff395bd435b888b89cb2b47038d8
AdelChernyatov <adel160203@mail.ru>
2022-05-23 23:37:06 +0300

    Полностью исправно работают методы зазрузки в бд паролей-картиной и обычных паролей.

--- PasswordGenerator/SqlConnection.cs
+++ PasswordGenerator/SqlConnection.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -21,26 +22,28 @@ namespace PasswordGenerator
              using (SQLiteConnection connection = new SQLiteConnection("Data Source = password.db;Version=3;"))
              {
                 connection.Open();
-                SqliteCommand com = new SqliteCommand($"Insert into Passwd_generation(ID,login,password) values(@id,@login,@password)");
-                com.Parameters.Add("@id", (SqliteType)DbType.Int32).Value = logpass.Id;
-                com.Parameters.Add("@login", (SqliteType)DbType.String).Value = logpass.Login;
-                com.Parameters.Add("@password", (SqliteType)DbType.String).Value = logpass.Password;
+                SQLiteCommand com = connection.CreateCommand();
+                com.CommandText = $"Insert into Passwd_generation(ID,login,password) values(@id,@login,@password)";
+                com.Parameters.Add("@id", DbType.Int32).Value = logpass.Id;
+                com.Parameters.Add("@login",DbType.String).Value = logpass.Login;
+                com.Parameters.Add("@password",DbType.String).Value = logpass.Password;
                 com.ExecuteNonQuery();
                 connection.Close();
              }
         }
         public void LoadImageToSql(ImagePassword imgpas)
         {    
-            MemoryStream ms = new MemoryStream(); 
-            SqliteConnection con = new SqliteConnection("Data Source=password.db;Version=3;");
+            MemoryStream ms = new MemoryStream();
+            imgpas.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            SQLiteConnection con = new SQLiteConnection("Data Source=password.db;Version=3;");
             con.Open();
-            SqliteCommand com = new SqliteCommand("insert into Image_password(id,image,password) values(@id,@photo,@password)", con);
-            com.Parameters.Add("@photo", (SqliteType)DbType.Binary, 10000).Value =imgpas.Image ;
-            com.Parameters.Add("@id", (SqliteType)DbType.Int32).Value =imgpas.Id;
-            com.Parameters.Add("@password", (SqliteType)DbType.String).Value =imgpas.Password;
+            SQLiteCommand com = new SQLiteCommand("insert into Image_password(id,image,password) values(@id,@photo,@password)", con);
+            com.Parameters.Add("@photo", DbType.Binary, 10000).Value =ms.ToArray();
+            com.Parameters.Add("@id", DbType.Int32).Value =imgpas.Id;
+            com.Parameters.Add("@password",DbType.String).Value =imgpas.Password;
             com.ExecuteNonQuery();
             con.Close();
-
         }
+        
     }
 }
