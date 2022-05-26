90a5a5a00d8e5adc5f952ace57c2a9736dee0b9c
Никита Макурин <makurin.nik@list.ru>
2022-05-26 18:40:05 +0300

    Тесты

--- PasswordGenerator/Algorythms.cs
+++ PasswordGenerator/Algorythms.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace PasswordGenerator
 {
-    internal static class Algorythms
+    public static class Algorythms
     {
         public static Color ChangeColorBrightness(Color color, double correctionFactor)
         {
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Drawing;
 
 namespace PasswordGenerator
 {
@@ -51,12 +52,12 @@ namespace PasswordGenerator
         {
             var passwordGenerator = new PasswordGenerator();
             passwordGenerator.UseNumbers = true;
-            passwordGenerator.PasswordLength = 1000;
+            passwordGenerator.PasswordLength = 5;
 
             bool excepted = false;
             bool actual = false;
             var result = passwordGenerator.Generate();
-            result.Replace("0", "");
+            result=result.Replace("0", "");
             if(!int.TryParse(result, out int value) || result.Length <= 0)
             {
                 actual = true;
@@ -79,13 +80,141 @@ namespace PasswordGenerator
         public void ExceptedEmptyWithoutParams()
         {
             var passwordGenerator = new PasswordGenerator();
-            passwordGenerator.PasswordLength = 1000;
+            passwordGenerator.PasswordLength = 100;
 
             var excepted = string.Empty;
             var result = passwordGenerator.Generate();
             var actual = result;
             Assert.AreEqual(excepted, actual);
         }
+        [Test]
+        public void EncryptAndDecrypt()
+        {
+            var excepted = "Леня";
+            var actual = Algorythms.EncryptString(excepted, excepted);
+            actual=Algorythms.DecryptString(actual, excepted);
+            Assert.AreEqual(excepted, actual);
+        }
+        [Test]
+        public void DecryptAndEncypt()
+        {
+            var excepted = "Леня";
+            excepted = Algorythms.EncryptString(excepted, "123");
+            var actual = Algorythms.DecryptString(excepted, "123");
+            actual = Algorythms.EncryptString(actual, excepted);
+            Assert.AreEqual(excepted.Length, actual.Length);
+        }
+        [Test]
+        public void NotContainsStrangeSymbols()
+        {
+            var passwordGenerator = new PasswordGenerator();
+            passwordGenerator.UseNumbers = true;
+            passwordGenerator.UseNonAlphanumeric = true;
+            passwordGenerator.ExcludeSimilar = true;
+            passwordGenerator.ExcludeAmbiguous = false;
+            passwordGenerator.PasswordLength = 500;
+             string ambiguousChars = "{}[]()/\\'\"`~,;:.<>";
+            var excepted = false;
+            var actual = false;
+            var result = passwordGenerator.Generate();
+            for (int i = 0; i < ambiguousChars.Length; i++)
+            {
+                if (!result.Contains(ambiguousChars[i]))
+                {
+                    actual = true;
+                }
+            }
+            
+            Assert.AreEqual(excepted, actual);
+        }
+        [Test]
+        public void NotContainsSimilarSymbols()
+        {
+            var passwordGenerator = new PasswordGenerator();
+            passwordGenerator.UseNumbers = true;
+            passwordGenerator.UseNonAlphanumeric = true;
+            passwordGenerator.ExcludeSimilar = false;
+            passwordGenerator.ExcludeAmbiguous = true;
+            passwordGenerator.PasswordLength = 500;
+            string similarChars = "iIl1Lo0O";
+            var excepted = false;
+            var actual = true;
+            var result = passwordGenerator.Generate();
+            for (int i = 0; i < similarChars.Length; i++)
+            {
+                if (result.Contains(similarChars[i]))
+                {
+                    actual = false;
+                }
+            }
+
+            Assert.AreEqual(excepted, actual);
+        }
+        [Test]
+        public void NotContainsSymbols()
+        {
+            var passwordGenerator = new PasswordGenerator();
+            passwordGenerator.UseNumbers = true;
+            passwordGenerator.UseNonAlphanumeric = false ;
+            passwordGenerator.ExcludeSimilar = true; ;
+            passwordGenerator.ExcludeAmbiguous = true;
+            passwordGenerator.PasswordLength = 500;
+            string similarChars = "~`'!@#$%^&*()_-+={}[]|\\:\";<,>.?/";
+            var excepted = false;
+            var actual = false;
+            var result = passwordGenerator.Generate();
+            for (int i = 0; i < similarChars.Length; i++)
+            {
+                if (result.Contains(similarChars[i]))
+                {
+                    actual = true;
+                }
+            }
+
+            Assert.AreEqual(excepted, actual);
+        }
+        [Test]
+        public void CheckColorChange()
+        {
+            Color excepted = Color.White;
+            Color actual = Algorythms.ChangeColorBrightness(Color.Green, 1);
+            Assert.AreEqual(excepted.ToArgb(), actual.ToArgb());
+
+        }
+        [Test]
+        public void CheckColorChangeWuthNegativeBright()
+        {
+            Color excepted = Color.FromArgb(255,0,0,0);//Red           
+            Color actual = Algorythms.ChangeColorBrightness(Color.Green, -1);
+            Assert.AreEqual(excepted.ToArgb(), actual.ToArgb());
+
+        }
+        [Test]
+        public void CheckLenLoginPassword()
+        {
+            var excepted = "difidufjifugjiufgj";
+            excepted = Algorythms.EncryptString(excepted,"123");
+            LoginPassword password = new LoginPassword(1, "Леня", excepted);
+            Assert.AreEqual(excepted.Length, password.Password.Length);
+
+        }
+        [Test]
+        public void CheckLenLoginIdPassword()
+        {
+            var excepted = 1;
+            LoginPassword password = new LoginPassword(excepted, "Леня", "djfidhfiufdjg");
+            Assert.AreEqual(excepted, password.Id);
+
+        }
+        [Test]
+        public void CheckLoginIdPassword()
+        {
+            var excepted = "Леня";
+            LoginPassword password = new LoginPassword(1, excepted, "djfidhfiufdjg");
+            Assert.AreEqual(excepted, password.Login);
+
+        }
+
 
     }
     
